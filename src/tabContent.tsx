ecfedc9d4461c165e368b03b72d99d3ccf10c296
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2022-02-14 22:19:23 +0100

    style: Format with prettier

--- src/tabContent.tsx
+++ src/tabContent.tsx
@@ -1,149 +1,213 @@
-import "./tabContent.scss"
+import './tabContent.scss';
 
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-import * as SDK from "azure-devops-extension-sdk"
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as SDK from 'azure-devops-extension-sdk';
 
-import { getClient } from "azure-devops-extension-api"
-import { Build, BuildRestClient, Attachment } from "azure-devops-extension-api/Build"
+import { getClient } from 'azure-devops-extension-api';
+import {
+  Build,
+  BuildRestClient,
+  Attachment,
+} from 'azure-devops-extension-api/Build';
 
-import { ObservableValue, ObservableObject } from "azure-devops-ui/Core/Observable"
-import { Observer } from "azure-devops-ui/Observer"
-import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs"
+import {
+  ObservableValue,
+  ObservableObject,
+} from 'azure-devops-ui/Core/Observable';
+import { Observer } from 'azure-devops-ui/Observer';
+import { Tab, TabBar, TabSize } from 'azure-devops-ui/Tabs';
 
+const ATTACHMENT_TYPE = 'report-html';
 
-const ATTACHMENT_TYPE = "report-html";
-
-SDK.init()
+SDK.init();
 SDK.ready().then(() => {
   try {
-    const config = SDK.getConfiguration()
+    const config = SDK.getConfiguration();
     config.onBuildChanged((build: Build) => {
-      let buildAttachmentClient = new BuildAttachmentClient(build)
-      buildAttachmentClient.init().then(() => {
-        displayReports(buildAttachmentClient)
-      }).catch(error => {throw new Error(error)})
-    })
-  } catch(error) {
-    throw new Error(error)
+      let buildAttachmentClient = new BuildAttachmentClient(build);
+      buildAttachmentClient
+        .init()
+        .then(() => {
+          displayReports(buildAttachmentClient);
+        })
+        .catch(error => {
+          throw new Error(error);
+        });
+    });
+  } catch (error) {
+    throw new Error(error);
   }
-})
+});
 
 function displayReports(attachmentClient: AttachmentClient) {
-  ReactDOM.render(<TaskAttachmentPanel attachmentClient={attachmentClient} />, document.getElementById("html-report-extention-container"))
+  ReactDOM.render(
+    <TaskAttachmentPanel attachmentClient={attachmentClient} />,
+    document.getElementById('html-report-extention-container'),
+  );
 }
 
 abstract class AttachmentClient {
-  protected attachments: Attachment[] = []
-  protected authHeaders: Object = undefined
-  protected reportHtmlContent: string = undefined
+  protected attachments: Attachment[] = [];
+  protected authHeaders: Object = undefined;
+  protected reportHtmlContent: string = undefined;
   constructor() {}
 
-  public getAttachments() : Attachment[] {
-    return this.attachments
+  public getAttachments(): Attachment[] {
+    return this.attachments;
   }
 
   public getDownloadableAttachment(attachmentName: string): Attachment {
-    const attachment = this.attachments.find((attachment) => { return attachment.name === attachmentName})
-    if (!(attachment && attachment._links && attachment._links.self && attachment._links.self.href)) {
-      throw new Error("Attachment " + attachmentName + " is not downloadable")
+    const attachment = this.attachments.find(attachment => {
+      return attachment.name === attachmentName;
+    });
+    if (
+      !(
+        attachment &&
+        attachment._links &&
+        attachment._links.self &&
+        attachment._links.self.href
+      )
+    ) {
+      throw new Error('Attachment ' + attachmentName + ' is not downloadable');
     }
-    return attachment
+    return attachment;
   }
 
   public async getAttachmentContent(attachmentName: string): Promise<string> {
     if (this.authHeaders === undefined) {
-      console.log('Get access token')
-      const accessToken = await SDK.getAccessToken()
-      const b64encodedAuth = Buffer.from(':' + accessToken).toString('base64')
-      this.authHeaders = { headers: {'Authorization': 'Basic ' + b64encodedAuth} }
+      console.log('Get access token');
+      const accessToken = await SDK.getAccessToken();
+      const b64encodedAuth = Buffer.from(':' + accessToken).toString('base64');
+      this.authHeaders = {
+        headers: { Authorization: 'Basic ' + b64encodedAuth },
+      };
     }
-    console.log("Get " + attachmentName + " attachment content")
-    const attachment = this.getDownloadableAttachment(attachmentName)
-    const response = await fetch(attachment._links.self.href, this.authHeaders)
+    console.log('Get ' + attachmentName + ' attachment content');
+    const attachment = this.getDownloadableAttachment(attachmentName);
+    const response = await fetch(attachment._links.self.href, this.authHeaders);
     if (!response.ok) {
-      throw new Error(response.statusText)
+      throw new Error(response.statusText);
     }
-    const responseText = await response.text()
-    console.log(responseText)
-    return responseText
+    const responseText = await response.text();
+    console.log(responseText);
+    return responseText;
   }
-  
 }
 
 class BuildAttachmentClient extends AttachmentClient {
-  private build: Build
+  private build: Build;
 
   constructor(build: Build) {
-    super()
-    this.build = build
+    super();
+    this.build = build;
   }
 
   public async init() {
-    const buildClient: BuildRestClient = getClient(BuildRestClient)
-    this.attachments = await buildClient.getAttachments(this.build.project.id, this.build.id, ATTACHMENT_TYPE)
-    console.log(this.attachments)
+    const buildClient: BuildRestClient = getClient(BuildRestClient);
+    this.attachments = await buildClient.getAttachments(
+      this.build.project.id,
+      this.build.id,
+      ATTACHMENT_TYPE,
+    );
+    console.log(this.attachments);
   }
 }
 
-
 interface TaskAttachmentPanelProps {
-  attachmentClient: AttachmentClient
+  attachmentClient: AttachmentClient;
 }
 
 export default class TaskAttachmentPanel extends React.Component<TaskAttachmentPanelProps> {
-  private selectedTabId: ObservableValue<string>
-  private tabContents: ObservableObject<string>
-  private tabInitialContent: string = '<div class="wide"><p>Loading...</p></div>'
+  private selectedTabId: ObservableValue<string>;
+  private tabContents: ObservableObject<string>;
+  private tabInitialContent: string =
+    '<div class="wide"><p>Loading...</p></div>';
 
   constructor(props: TaskAttachmentPanelProps) {
     super(props);
-    this.selectedTabId = new ObservableValue(props.attachmentClient.getAttachments()[0].name)
-    this.tabContents = new ObservableObject()
+    this.selectedTabId = new ObservableValue(
+      props.attachmentClient.getAttachments()[0].name,
+    );
+    this.tabContents = new ObservableObject();
   }
 
   public escapeHTML(str: string) {
-    return str.replace(/[&<>'"]/g, tag => ({
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      "'": '&#39;',
-      '"': '&quot;'
-    }[tag] || tag))
+    return str.replace(
+      /[&<>'"]/g,
+      tag =>
+        ({
+          '&': '&amp;',
+          '<': '&lt;',
+          '>': '&gt;',
+          "'": '&#39;',
+          '"': '&quot;',
+        }[tag] || tag),
+    );
   }
 
   public render() {
-    const attachments = this.props.attachmentClient.getAttachments()
+    const attachments = this.props.attachmentClient.getAttachments();
     if (attachments.length == 0) {
-      return (null)
+      return null;
     } else {
-      const tabs = []
+      const tabs = [];
       for (const attachment of attachments) {
-        const metadata = attachment.name.split('.')
+        const metadata = attachment.name.split('.');
         // Conditionally add counter for multistage pipeline
-        const name = metadata[2] !== '__default' ? `${metadata[2]} #${metadata[3]}` : metadata[0]
+        const name =
+          metadata[2] !== '__default'
+            ? `${metadata[2]} #${metadata[3]}`
+            : metadata[0];
 
-        tabs.push(<Tab name={name} id={attachment.name} key={attachment.name} url={attachment._links.self.href}/>)
-        this.tabContents.add(attachment.name, this.tabInitialContent)
+        tabs.push(
+          <Tab
+            name={name}
+            id={attachment.name}
+            key={attachment.name}
+            url={attachment._links.self.href}
+          />,
+        );
+        this.tabContents.add(attachment.name, this.tabInitialContent);
       }
       return (
         <div className="flex-column">
-          { attachments.length > 0 ?
+          {attachments.length > 0 ? (
             <TabBar
               onSelectedTabChanged={this.onSelectedTabChanged}
               selectedTabId={this.selectedTabId}
-              tabSize={TabSize.Tall}>
+              tabSize={TabSize.Tall}
+            >
               {tabs}
             </TabBar>
-          : null }
-          <Observer selectedTabId={this.selectedTabId} tabContents={this.tabContents}>            
+          ) : null}
+          <Observer
+            selectedTabId={this.selectedTabId}
+            tabContents={this.tabContents}
+          >
             {(props: { selectedTabId: string }) => {
-              if ( this.tabContents.get(props.selectedTabId) === this.tabInitialContent) {
-                this.props.attachmentClient.getAttachmentContent(props.selectedTabId).then((content) => {
-                  this.tabContents.set(props.selectedTabId, '<iframe class="wide flex-row flex-center" srcdoc="' + this.escapeHTML(content) + '"></iframe>')
-              })
-            }
-              return  <span dangerouslySetInnerHTML={ {__html: this.tabContents.get(props.selectedTabId)} } />
+              if (
+                this.tabContents.get(props.selectedTabId) ===
+                this.tabInitialContent
+              ) {
+                this.props.attachmentClient
+                  .getAttachmentContent(props.selectedTabId)
+                  .then(content => {
+                    this.tabContents.set(
+                      props.selectedTabId,
+                      '<iframe class="wide flex-row flex-center" srcdoc="' +
+                        this.escapeHTML(content) +
+                        '"></iframe>',
+                    );
+                  });
+              }
+              return (
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: this.tabContents.get(props.selectedTabId),
+                  }}
+                />
+              );
             }}
           </Observer>
         </div>
@@ -153,8 +217,5 @@ export default class TaskAttachmentPanel extends React.Component<TaskAttachmentP
 
   private onSelectedTabChanged = (newTabId: string) => {
     this.selectedTabId.value = newTabId;
-  }
+  };
 }
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,58 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-       tabContent:  "./src/tabContent.tsx",
-    },
+  entry: {
+    tabContent: './src/tabContent.tsx',
+  },
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {
-            "azure-devops-extension-sdk": path.resolve("node_modules/azure-devops-extension-sdk")
-        },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      'azure-devops-extension-sdk': path.resolve(
+        'node_modules/azure-devops-extension-sdk',
+      ),
     },
-    stats: {
-        warnings: false
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "azure-devops-ui/buildScripts/css-variables-loader", "sass-loader"]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.woff$/,
-                use: [{
-                    loader: 'base64-inline-loader'
-                }]
-            },
-            {
-                test: /\.html$/,
-                loader: "file-loader"
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin({patterns: [
-            "src/*.html"
-        ]}),
-        new webpack.SourceMapDevToolPlugin({})
-    ]
+  },
+  stats: {
+    warnings: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'azure-devops-ui/buildScripts/css-variables-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff$/,
+        use: [
+          {
+            loader: 'base64-inline-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: ['src/*.html'] }),
+    new webpack.SourceMapDevToolPlugin({}),
+  ],
 };
